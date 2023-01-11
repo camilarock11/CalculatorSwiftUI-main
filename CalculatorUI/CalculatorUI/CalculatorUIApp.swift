40fd276b1ec7b598fe28bef6bc21190c1c8476e9
t775343 <camila.roque@f1rst.com.br>
2023-01-10 21:16:16 -0300

    Initial commit

---
+++ CalculatorUI/CalculatorUI/CalculatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUIApp.swift
+//  CalculatorUI
+//
+//  Created by Camila Roque on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
